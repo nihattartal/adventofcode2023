a4eae9e9e3a53fa7514d93de25488c475b32a349
Nihat Tartal <ntartal@flexport.com>
2023-12-17 06:44:05 +0000

    Day 14 - Part 1

---
+++ 14/solution.js
@@ -0,0 +1,45 @@
+import fs from "fs";
+import appRoot from "app-root-path"
+import _ from "lodash"
+
+const fileInput = fs.readFileSync(`${appRoot}/inputs/14/input.txt`, { encoding: 'utf8', flag: 'r' });
+const data = fileInput.split("\n").map(x => x.trim()).filter(x => !!x);
+
+const matrix = data.map(x => x.split(''));
+
+matrix.forEach((row, rowIndex) => {
+  return row.forEach((col, colIndex) => {
+    if (col == ".") {
+      const foundRockIndex = findNextRowIndexWithRock(rowIndex, colIndex);
+      if (foundRockIndex !== -1) {
+        matrix[foundRockIndex][colIndex] = ".";
+        matrix[rowIndex][colIndex] = "O";
+      }
+    }
+  })
+})
+
+function findNextRowIndexWithRock(row, col) {
+  let found = -1;
+  for (let i = row + 1; i < matrix.length; i++) {
+    const foundItem = matrix[i][col];
+    if (foundItem == "O") {
+      return i;
+    } else if (foundItem == "#") {
+      return found;
+    }
+  }
+  return found;
+}
+
+const part1 = matrix.reverse().reduce((res, row, rowIndex) => {
+  const realRowIndex = rowIndex + 1;
+  const count = row.reduce((rowCount, col) => {
+    if (col == "O") {
+      return rowCount + 1;
+    }
+    return rowCount;
+  }, 0);
+  return res + (count * realRowIndex);
+}, 0);
+console.log(`Part 1: ${part1}`);
